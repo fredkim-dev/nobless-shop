a01bb299595c619e5716731923f5d0194c37e4a2
Fred KIM <chez.molton@gmail.com>
2020-04-14 17:16:22 +0200

    Using slick as carousel in homepage
    - Add new templates to hide others pages
    - New bootstrap breakpoints
    - Use javascript functions file

--- themes/NoblessTheme/assets/entry.js
+++ themes/NoblessTheme/assets/entry.js
@@ -1,61 +1,14 @@
 /* JS plugins */
 import 'popper.js/dist/popper.min';
-import 'slick-carousel/slick/slick.min';
 import '@fortawesome/fontawesome-free/js/all';
 import 'bootstrap/dist/js/bootstrap.min';
+import { init, resizeContent } from './functions';
 import './indexBootstrapTheme';
+
 /* Css files */
 import './scss/index.scss';
 /* Media files */
 import './media/logo.png';
-
-/**
- * Resize function to keep all the main-content visible
- */
-function resizeContent() {
-    const footerHeight = document.getElementById('footerContainer').clientHeight;
-    const contentHeight = window.innerHeight - document.getElementById('headerContainer').clientHeight - footerHeight;
-    document.getElementById('mainContentContainer').style.height = contentHeight + "px";
-}
-
-/**
- * Carousel creation
- */
-function createCarousel() {
-  jQuery('#carouselLatest').slick({
-    infinite: true,
-    speed: 700,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    appendArrows: jQuery('#carouselLatest').next("#slider-controls"),
-    prevArrow: '<div class="div-btn"><button type="button" class="slick-prev ">Previous</button></div>',
-    nextArrow: '<div class="div-btn"><button type="button" class="slick-next">Previous</button></div>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-}
 
 /**
  * Manage dropdown on mobile size
@@ -69,11 +22,6 @@ if (menuDropdown) {
   menuDropdown.addEventListener('hidden.bs.collapse', function(e){
     resizeContent();
   }, false);
-}
-
-function init() {
-  createCarousel();
-  resizeContent();
 }
 
 window.onload = init;
---
+++ themes/NoblessTheme/assets/functions.js
@@ -0,0 +1,106 @@
+import 'slick-carousel/slick/slick.min';
+
+/**
+ * Resize function to keep all the main-content visible
+ */
+function resizeContent() {
+  const footerHeight = document.getElementById('footerContainer').clientHeight;
+  const contentHeight = window.innerHeight - document.getElementById('headerContainer').clientHeight - footerHeight;
+  document.getElementById('mainContentContainer').style.height = contentHeight + "px";
+}
+
+/**
+ * Carousel creation
+ */
+function createCarousel() {
+  jQuery('#carouselLatestProducts').slick({
+    infinite: true,
+    speed: 700,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    appendArrows: jQuery('#carouselLatestProducts').next('.slider-controls'),
+    prevArrow: '<div class="div-btn"><button type="button" class="slick-prev ">Previous</button></div>',
+    nextArrow: '<div class="div-btn"><button type="button" class="slick-next">Previous</button></div>',
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  jQuery('#carouselLatestBundles').slick({
+    infinite: true,
+    speed: 700,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    appendArrows: jQuery('#carouselLatestBundles').next('.slider-controls'),
+    prevArrow: '<div class="div-btn"><button type="button" class="slick-prev ">Previous</button></div>',
+    nextArrow: '<div class="div-btn"><button type="button" class="slick-next">Previous</button></div>',
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+}
+
+function init() {
+  createCarousel();
+  resizeContent();
+}
+
+export { init, resizeContent };
